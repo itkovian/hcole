9452a2c916455448f04a7d235fcbd69425669c78
Andy Georges <andy.georges@elis.ugent.be>
2011-08-31 13:23:33 +0200

    Restart :-)

--- README.md
+++
@@ -1,6 +0,0 @@
-This is the repository for the effort of rewriting the COLE exploration
-framework in Haskell. The main purpose is to be as modular as possible.
-
-The repository contains multiple packages:
-- Spea2: the basic SPEA2 algorithm, that is used throughout the COLE framework
-  as the genetic algorithm of choice
---
+++ src/hCole.hs
@@ -0,0 +1,25 @@
+{-
+
+Entrypoint to the Haskell COLE implementation.
+
+-}
+
+module Main 
+  (
+  )where
+
+import System.Console.CmdArgs
+
+
+--------------------------------------------------------------------------------
+-- data structure for the command line arguments
+data ColeCmdArgs = ColeCmdArgs 
+    { 
+    
+    }
+
+
+
+main = do 
+  
+  
